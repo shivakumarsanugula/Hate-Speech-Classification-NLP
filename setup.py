0f85e7ae8176ff7cf2587b99d3c89db369020658
shivakumarsanugula <shivakumaryadav1997@gmail.com>
2023-12-19 17:26:24 +0530

    updated code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="SHIVA KUMAR",
+    author_email="shivakumaryadav1997@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
